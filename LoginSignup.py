a6526be575db249c385127fe4ddc22f11e670d24
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-06-01 09:35:59 +0530

    new puts chat box

--- LoginSignup.py
+++ LoginSignup.py
@@ -12,3 +12,8 @@ def signup():
     instruction.grid(row=0,column=0,sticky=E)
 
     namel=Label(roots,text="New Username")
+    pwordl=Label(roots,text="New Password")
+    namel.grid(row=1,column=1,sticky=w)
+    pwordl.grid(row=2,column=0,sticky=w)
+
+    nameE=Entry(roots)
