1b4bac36a9df19c6fc7af30d11218d7255e0f74d
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-06-01 09:40:51 +0530

    creating window

--- LoginSignup.py
+++ LoginSignup.py
@@ -17,3 +17,10 @@ def signup():
     pwordl.grid(row=2,column=0,sticky=w)
 
     nameE=Entry(roots)
+    pwordE=Entry(roots,show='*')
+    nameE.grid(row=1, column=1)
+    pwordE.grid(row=2, column=1)
+
+    signupButton=Button(roots,text="Signup",command=FSSignup)
+    signupButton.grid(columnspan=2,sticky=w)
+    root.mainloop()
